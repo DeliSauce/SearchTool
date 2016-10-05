ad63bb59e63b12ac8f2456e15912bdce70f188b1
DeliSauce <nycdelisauce@gmail.com>
2016-10-05 00:06:37 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-searchGoogle = function(word){
+searchSites = function(word){
   var query = word.selectionText;
   chrome.tabs.create({url: "https://www.google.com/search?q=" + query});
   chrome.tabs.create({url: "https://duckduckgo.com/?q=!ducky" + query});
@@ -6,6 +6,6 @@ searchGoogle = function(word){
 
 chrome.contextMenus.create({
   title: "Open Article in New Tab",
-  contexts:["selection"],
-  onclick: searchGoogle
+  contexts:["selection", "editable"],
+  onclick: searchSites
 });
