4da2556cd80a73f8c72cfd682115535e173feab8
DeliSauce <nycdelisauce@gmail.com>
2016-10-05 00:01:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-searchUrbanDict = function(word){
+searchGoogle = function(word){
   var query = word.selectionText;
-  chrome.tabs.create({url: "http://www.urbandictionary.com/define.php?term=" + query});
+  chrome.tabs.create({url: "https://www.google.com/search?q=" + query});
+  chrome.tabs.create({url: "https://duckduckgo.com/?q=!ducky" + query});
 };
 
 chrome.contextMenus.create({
-  title: "Search in UrbanDictionary",
+  title: "Open Article in New Tab",
   contexts:["selection"],
-  onclick: searchUrbanDict
+  onclick: searchGoogle
 });
